e0b278a5bbdf66e74cd17c98b405cbd18fa1737f
Kartavchenko <97044264+Kartavchenko@users.noreply.github.com>
2022-08-20 18:42:54 +0000

    Deploying to gh-pages from @ Kartavchenko/manufacture-mk-1@0248d526c3e1389c9be6e9638e26a98ca4cbab9c 🚀

---
+++ index.e876b85c.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),a=document.querySelector("[data-nav]");e.addEventListener("click",(function n(){e.getAttribute("aria-expanted"),e.classList.toggle("is-active"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open"),a.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.e876b85c.js.map
---
+++ index.ffcf473a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),a=document.querySelector("[data-nav]");e.addEventListener("click",(function n(){e.getAttribute("aria-expanted"),e.classList.toggle("is-active"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open"),a.classList.toggle("is-open")}));
+//# sourceMappingURL=index.ffcf473a.js.map
